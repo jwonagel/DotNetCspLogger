f5870c6c9355a9a73193e0b17ad3eedd9b0ce0f3
Jwo Nagel <jwo.nagel@egeli.com>
2022-02-17 11:24:24 +0100

    Update to .NET6

--- src/Program.cs
+++ src/Program.cs
@@ -1,40 +1,70 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Microsoft.OpenApi.Models;
 using Serilog;
 
-namespace csplogger
+var builder = WebApplication.CreateBuilder(args);
+
+var conf = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables()
+    .Build();
+
+var path = conf.GetValue<string>("LogPath");
+
+builder.Host.UseSerilog((context, services, configuration) => configuration
+    .ReadFrom.Configuration(context.Configuration)
+    .ReadFrom.Services(services)
+    .WriteTo.Console()
+    .WriteTo.File(path,
+        rollingInterval: RollingInterval.Day,
+        rollOnFileSizeLimit: true));
+
+builder.Services.AddControllers(conf =>
 {
-    public class Program
+    conf.InputFormatters.OfType<SystemTextJsonInputFormatter>().First().SupportedMediaTypes.Add(
+        MediaTypeHeaderValue.Parse("application/csp-report")
+    );
+});
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "csplogger", Version = "v1" });
+});
+
+var app = builder.Build();
+
+
+app.UseDeveloperExceptionPage();
+app.UseSwagger();
+var virtualPath = conf.GetValue<string>("VirtualPath") ?? string.Empty;
+virtualPath = EnsureTrailingSlash(virtualPath);
+
+app.UseSwaggerUI(c => c.SwaggerEndpoint($"{virtualPath}swagger/v1/swagger.json", "csplogger v1"));
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+app.Run();
+
+string EnsureTrailingSlash(string input)
+{
+    if (!input.EndsWith("/"))
     {
-        public static void Main(string[] args)
-        {
-            var conf = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
-
-            var path = conf.GetValue<string>("LogPath");
-            
-            CreateHostBuilder(args, path).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args, string path) =>
-            Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, services, configuration) => configuration
-                    .ReadFrom.Configuration(context.Configuration)
-                    .ReadFrom.Services(services)
-                    .WriteTo.Console()
-                    .WriteTo.File(path,
-                rollingInterval: RollingInterval.Day,
-                rollOnFileSizeLimit: true))
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        return input + "/";
     }
+
+    return input;
 }
